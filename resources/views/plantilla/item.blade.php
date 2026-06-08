99fa3f48546b15b95c75ed7df932d380f2b65f10
Antonio Peña <angeles_yanco@hotmail.com>
2026-06-08 17:20:00 -0600

    Organiza la plantilla AdminLTE4. Sub006.

---
+++ resources/views/plantilla/item.blade.php
@@ -0,0 +1,27 @@
+@extends('plantilla.app')
+@section('content')
+<!--begin::Container-->
+<div class="container-fluid">
+    <!--begin::Row-->
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card mb-4">
+                <div class="card-header">
+                    <h3 class="card-title">Listado</h3>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+
+                </div>
+                <!-- /.card-body -->
+                <div class="card-footer clearfix">
+
+                </div>
+            </div>
+            <!-- /.card -->
+        </div>
+    </div>
+    <!--end::Row-->
+</div>
+<!--end::Container-->
+@endsection
