4ebba998aeb490ee54bcd675db153c7aae33f57b
Antonio Peña <angeles_yanco@hotmail.com>
2026-06-09 17:48:04 -0600

    Agrega DataTables a la pantalla. Sub008

--- resources/views/plantilla/item.blade.php
+++ resources/views/plantilla/item.blade.php
@@ -1,4 +1,7 @@
 @extends('plantilla.app')
+@push('estilos')
+<link rel="stylesheet" href="{{ asset('datatables/dataTables.bootstrap5.css') }}">
+@endpush
 @section('content')
 <!--begin::Container-->
 <div class="container-fluid">
@@ -11,7 +14,67 @@
                 </div>
                 <!-- /.card-header -->
                 <div class="card-body">
-
+                    <div class="table-responsive">
+                        <table id="tablaListado" class="table table-striped table-hover table-sm">
+                            <thead>
+                                <th>ID</th>
+                                <th>Nombre</th>
+                                <th>Descripción</th>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>NIU-1</td>
+                                    <td>Unidades</td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>NIU-2</td>
+                                    <td>Unidades</td>
+                                </tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td>NIU-3</td>
+                                    <td>Unidades</td>
+                                </tr>
+                                <tr>
+                                    <td>4</td>
+                                    <td>NIU-4</td>
+                                    <td>Unidades</td>
+                                </tr>
+                                <tr>
+                                    <td>5</td>
+                                    <td>NIU-5</td>
+                                    <td>Unidades</td>
+                                </tr>
+                                <tr>
+                                    <td>6</td>
+                                    <td>NIU-6</td>
+                                    <td>Unidades</td>
+                                </tr>
+                                <tr>
+                                    <td>7</td>
+                                    <td>NIU-7</td>
+                                    <td>Unidades</td>
+                                </tr>
+                                <tr>
+                                    <td>8</td>
+                                    <td>NIU-8</td>
+                                    <td>Unidades</td>
+                                </tr>
+                                <tr>
+                                    <td>9</td>
+                                    <td>NIU-9</td>
+                                    <td>Unidades</td>
+                                </tr>
+                                <tr>
+                                    <td>10</td>
+                                    <td>NIU-10</td>
+                                    <td>Unidades</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
                 <!-- /.card-body -->
                 <div class="card-footer clearfix">
@@ -25,3 +88,11 @@
 </div>
 <!--end::Container-->
 @endsection
+@push('scripts')
+<script src="{{ asset('datatables/jquery-3.7.1.js') }}"></script>
+<script src="{{ asset('datatables/dataTables.js') }}"></script>
+<script src="{{ asset('datatables/dataTables.bootstrap5.js') }}"></script>
+<script>
+    new DataTable('#tablaListado');
+</script>
+@endpush
