5f939f4092b965d8730ffc77f734880b85ee9ced
Antonio Peña <apena@porrua.com>
2026-07-01 10:49:43 -0600

    feat: Crea el control de rutas mediante middleware. Sub034

--- routes/web.php
+++ routes/web.php
@@ -10,28 +10,30 @@ use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::middleware(['guest'])->group(function(){
+    Route::get('/', function () {
+        return view('autenticacion.login');
+    });
+    Route::get('login', function(){
+        return view('autenticacion.login');
+    })->name('login');
+    Route::post('login', [AuthController::class, 'login'])->name('login.post');
 });
 
-Route::get('/', function () {
-    return view('plantilla.item');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/afectacion-tipos/select', [AfectacionTipoController::class, 'select'])->name('afectacion-tipos.select');
+    Route::get('/unidades/select', [UnidadController::class, 'select'])->name('unidades.select');
+    Route::get('/permisos/select', [RoleController::class, 'permisos'])->name('permisos.select');
+    Route::get('/roles/select', [RoleController::class, 'roles'])->name('roles.select');
+
+    Route::resource('unidades', UnidadController::class)->except(['create', 'edit']);
+    Route::resource('productos', ProductoController::class)->except(['create', 'edit']);
+    Route::resource('roles', RoleController::class)->except(['create', 'edit']);
+    Route::resource('usuarios', UserController::class)->except(['create', 'edit']);
+
+    Route::get('dashboard', function(){
+        return view('dashboard.index');
+    })->name('dashboard');
 });
-
-Route::get('/afectacion-tipos/select', [AfectacionTipoController::class, 'select'])->name('afectacion-tipos.select');
-Route::get('/unidades/select', [UnidadController::class, 'select'])->name('unidades.select');
-Route::get('/permisos/select', [RoleController::class, 'permisos'])->name('permisos.select');
-Route::get('/roles/select', [RoleController::class, 'roles'])->name('roles.select');
-
-Route::resource('unidades', UnidadController::class)->except(['create', 'edit']);
-Route::resource('productos', ProductoController::class)->except(['create', 'edit']);
-Route::resource('roles', RoleController::class)->except(['create', 'edit']);
-Route::resource('usuarios', UserController::class)->except(['create', 'edit']);
-
-Route::get('login', function(){
-    return view('autenticacion.login');
-})->name('login');
-Route::post('login', [AuthController::class, 'login'])->name('login.post');
-Route::get('dashboard', function(){
-    return view('dashboard.index');
-})->name('dashboard');
